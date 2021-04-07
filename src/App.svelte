bcf94f989c417e5b17fc9a17a1f5304e94f3fe7b
Jonny Thaw <hi@jthaw.me>
2021-04-07 21:52:45 +0100

    sorted the code a bit

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import { onMount, onDestroy, setContext } from "svelte";
   import { writable } from "svelte/store";
 
+  // Pretty arbitrary width/height
   const width = writable(600);
   const height = writable(600);
   const pixelRatio = writable(window.devicePixelRatio);
@@ -12,17 +13,22 @@
   let context: CanvasRenderingContext2D;
   let frame;
 
+  // Makes the palette out of the lovely chromotome library
   const colors = tome.get("saami");
 
+  // cell position
   let x = 0;
   let y = 0;
 
+  // Store a reference to the div that will show the dice roll
   let feedbackEl: HTMLDivElement;
 
+  // Reactive variables to deal with the cells and their sizes
   $: cols = 10;
   $: size = ($width * $pixelRatio) / cols;
   $: rows = Math.ceil(($height * $pixelRatio) / size);
 
+  // Little helper functions for size/circle circumference
   const s = (perc) => size * perc;
   const c = (perc) => perc * (Math.PI * 2);
 
@@ -37,11 +43,12 @@
     run();
   });
 
+  /**
+   * This is the function that kicks off the drawing
+   */
   function run() {
     x = 0;
     y = 0;
-
-    console.log("hey?", cols, size);
 
     if (fill) {
       context.save();
@@ -56,6 +63,7 @@
     frame = requestAnimationFrame(update);
   }
 
+  // Animation loop
   function update() {
     addDrawing();
 
@@ -64,6 +72,7 @@
     }
   }
 
+  // Draws the diagonal line
   function drawOne() {
     context.beginPath();
     context.moveTo(0, 0);
@@ -77,6 +86,7 @@
     }
   }
 
+  // Draws the circle bottom left
   function drawTwo() {
     context.beginPath();
     context.arc(s(0.25), s(0.75), s(0.25), 0, c(1));
@@ -89,6 +99,7 @@
     }
   }
 
+  // Draws the square top right
   function drawThree() {
     context.beginPath();
     context.rect(s(0.5), 0, s(0.5), s(0.5));
@@ -101,6 +112,7 @@
     }
   }
 
+  // Draws the half circle top left
   function drawFour() {
     context.beginPath();
     context.arc(s(0.25), s(0.25), s(0.25), c(0.125), c(0.625));
@@ -114,6 +126,7 @@
     }
   }
 
+  // Draws the half circle bottom right
   function drawFive() {
     context.beginPath();
     context.arc(s(0.75), s(0.75), s(0.25), c(0.625), c(0.125));
@@ -127,6 +140,7 @@
     }
   }
 
+  // Utility to help with the advancing of cells
   function advance() {
     x = x + 1;
 
@@ -136,10 +150,12 @@
     }
   }
 
+  // Clamp a value, wish this was just in javascript
   function clamp(val, min, max) {
     return Math.min(Math.max(val, min), max);
   }
 
+  // This is just for the visual on mobile, so the canvas scales with the device a bit
   function handleResize() {
     canvas.style.setProperty(
       "--scale-down",
@@ -152,10 +168,12 @@
   }
 
   function addDrawing() {
+    // Simulates 'rolling' a 6 sided dice
     const roll = Math.floor(Math.random() * 6) + 1;
 
     addMessage(roll);
 
+    // Super simple switch case to draw after the dice roll
     context.save();
     context.translate(x * size, y * size);
     context.lineWidth = size / 20;
@@ -187,73 +205,135 @@
   }
 
   function restartCols(e) {
+    // It gets funky if we dont restart the drawing after changing the columns
     cols = parseInt(e.target.value, 10);
     run();
   }
 </script>
 
 <main>
-  <canvas
-    bind:this={canvas}
-    width={$width * $pixelRatio}
-    height={$height * $pixelRatio}
-    style="width: {$width}px; height: {$height}px;"
-    on:click={addDrawing}
-  />
+  <div class="banner">
+    <div class="left">
+      <a href="https://github.com/jthawme/handmade-generative" target="_blank"
+        >Site code (made with Svelte)</a
+      >
+      <a href="https://jthaw.me/files/activity-sheet-2.pdf" target="_blank"
+        >Download PDF</a
+      >
+    </div>
 
-  <div class="feedback">
-    Last rolled: <span bind:this={feedbackEl} />
+    <div class="right">
+      <a href="https://jthaw.me/newsletter"
+        >This was made for my newsletter. Sign up for more like it</a
+      >
+    </div>
   </div>
+  <div class="wrapper">
+    <canvas
+      bind:this={canvas}
+      width={$width * $pixelRatio}
+      height={$height * $pixelRatio}
+      style="width: {$width}px; height: {$height}px;"
+      on:click={addDrawing}
+    />
 
-  <div class="actions">
-    <button on:click={toggleAutoplay}
-      >Autoplay: {autoplay ? "on" : "off"}</button
-    >
-    <label>
-      <button on:click={update} disabled={autoplay}>Next roll</button>
-    </label>
+    <div class="feedback">
+      Last rolled: <span bind:this={feedbackEl} />
+    </div>
 
-    <label>
-      <span>Columns ({cols})</span>
-      <input
-        type="range"
-        min="5"
-        max="100"
-        value="10"
-        on:change={restartCols}
-      />
-    </label>
+    <div class="actions">
+      <button on:click={toggleAutoplay}
+        >Autoplay: {autoplay ? "on" : "off"}</button
+      >
+      <label>
+        <button on:click={update} disabled={autoplay}>Next roll</button>
+      </label>
 
-    <label>
-      <span>Fill?</span>
-      <input type="checkbox" bind:checked={fill} />
-    </label>
+      <label>
+        <span>Columns ({cols})</span>
+        <input
+          type="range"
+          min="5"
+          max="100"
+          value="10"
+          on:change={restartCols}
+        />
+      </label>
 
-    <label>
-      <button on:click={run}>Restart</button>
-    </label>
+      <label>
+        <span>Fill?</span>
+        <input type="checkbox" bind:checked={fill} />
+      </label>
+
+      <label>
+        <button on:click={run}>Restart</button>
+      </label>
+    </div>
   </div>
 </main>
 
 <svelte:window on:resize|passive={handleResize} />
 
 <style>
-  * {
-    padding: 0;
-    margin: 0;
+  main {
+    display: flex;
+    flex-direction: column;
+
+    overflow: hidden;
+
+    min-height: 100vh;
   }
 
-  main {
+  .wrapper {
     display: grid;
     grid-template-columns: repeat(12, 1fr);
 
     max-width: 1400px;
+    gap: 10px;
     margin: 0 auto;
 
-    overflow: hidden;
+    flex-grow: 1;
 
     align-items: center;
   }
+
+  .banner {
+    display: flex;
+
+    background-color: #e7e6e4;
+    flex-direction: column;
+
+    margin-bottom: 2em;
+  }
+
+  .left {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+
+    flex-grow: 1;
+  }
+
+  .left a {
+    color: #333;
+    margin-right: 20px;
+    margin-bottom: 10px;
+  }
+
+  .right a {
+    display: flex;
+
+    align-items: center;
+    color: white;
+    background-color: #39638f;
+    padding: 10px 20px;
+  }
+
+  .right a:hover {
+    background-color: #e3b83e;
+    text-decoration: none;
+  }
+
   canvas {
     align-self: center;
     justify-self: center;
@@ -297,33 +377,58 @@
   }
 
   @media screen and (min-width: 768px) {
+    .banner {
+      margin-bottom: 5px;
+    }
+
+    .banner,
+    .left {
+      flex-direction: row;
+    }
+
+    .left a {
+      margin-bottom: 0;
+    }
+
     main {
       height: 100vh;
     }
 
+    .wrapper {
+      grid-template-columns: 600px 1fr;
+      width: 100%;
+    }
+
     canvas {
       grid-row: 1 / span 2;
-      grid-column: 1 / span 9;
+      grid-column: 1;
     }
 
     .feedback,
     .actions {
-      grid-column: 10 / span 3;
+      grid-column-end: span 1;
+    }
+
+    .feedback,
+    .actions {
+      grid-column-start: 2;
     }
   }
 
   @media screen and (min-width: 1000px) {
     canvas {
       grid-row: 1;
-      grid-column: 1 / span 6;
     }
 
-    .feedback {
-      grid-column: 7 / span 2;
+    .wrapper {
+      gap: 30px;
+      grid-template-columns: 600px 1fr 1fr;
     }
 
+    .feedback,
     .actions {
-      grid-column: 10 / span 2;
+      grid-column-start: unset;
+      grid-column-end: span 1;
     }
   }
 </style>
